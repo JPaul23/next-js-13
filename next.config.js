515e1419d9d772bc18310f0229cbae8a421d0ab1
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-11-01 16:12:06 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
